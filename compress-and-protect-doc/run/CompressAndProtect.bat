29e294650d47fee140fc76a889da099d3b87d32f
giuseppe.giordano1992@gmail.com <giuseppe.giordano1992@gmail.com>
2022-07-03 15:35:47 +0200

    primo run su mio pc

--- compress-and-protect-doc/run/CompressAndProtect.bat
+++ compress-and-protect-doc/run/CompressAndProtect.bat
@@ -1,3 +1,8 @@
+set JAVA_HOME=C:\Program Files\Java\openlogic-openjdk-8u332-b09-windows-64
+set path=%path%;%JAVA_HOME%\bin
+
+echo %path%
+
 for /f "delims=" %%x in ('dir /od /b compress-and-protect-doc-*-jar-with-dependencies.jar') do set latestjar=%%x
 echo %latestjar% 
 java -classpath config -jar %latestjar% ONLY_PROTECT
